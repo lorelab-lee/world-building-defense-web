5ba9804c82299c8f0b8a9c6ed6ed5b26af09ef90
이재홍 <jaehong@ijaehong-ui-MacBookPro.local>
2026-04-21 17:59:13 +0900

    deploy: 2026-04-21 17:59

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776760869|2178666';
+const CACHE_VERSION = '1776761950|2170839';
 /** @type {string} */
 const CACHE_PREFIX = '월드 빌딩 디펜스-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
